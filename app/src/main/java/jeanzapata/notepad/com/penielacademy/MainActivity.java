716c74706007929e2d7fe223e27775ac9232c334
JeanGonzalo <u17214770@utp.edu.pe>
2018-08-18 05:50:21 -0500

    third

--- app/src/main/java/jeanzapata/notepad/com/penielacademy/MainActivity.java
+++ app/src/main/java/jeanzapata/notepad/com/penielacademy/MainActivity.java
@@ -1,7 +1,9 @@
 package jeanzapata.notepad.com.penielacademy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToNextAct(View v)
+    {
+        Intent in = new Intent(MainActivity.this, PerfilActivity.class);
+        startActivity(in);
     }
 }
